1809ec391fe603764855e3cc3171ad45883f7bc5
MubarakOke <okeolamubarak18@gmail.com>
2022-10-30 17:35:08 +0100

    added the readme docs

--- frontend/src/Componentes/sensorData.js
+++ frontend/src/Componentes/sensorData.js
@@ -1,41 +1,46 @@
 import React, {useState, useEffect} from 'react'
 import {fetchData} from '../Utils/fetchData'
 
-function SensorData() {
-    const [data, setData] = React.useState([])
+function SensorData({interval}) {
+    const [data, setData] = useState([])
 
-    const renderData= async()=>{
-        return data.results.map((item, index)=>{
+    const renderData= (data)=>{
+        if (data.length==0)return (<div>Loading.............................</div>)
+        return data.map((item, index)=>{
             return  (
                     <div className="table-row" key={index}>
-                        <div class="table-cell tablet:text-[20px] text-[18px] break-words w-[33.33%] pl-[3px]">{item.sensor1}</div>
-                        <div class="table-cell tablet:text-[20px] text-[18px] break-words w-[33.33%] pl-[3px]">{item.sensor2}</div>
-                        <div class="table-cell tablet:text-[20px] text-[18px] break-words w-[33.33%] pl-[3px]">{item.sensor3}</div>
-                        <div class="table-cell tablet:text-[20px] text-[18px] break-words w-[33.33%] pl-[3px]">{item.sensor4}</div>
-                        <div class="table-cell tablet:text-[20px] text-[18px] break-words w-[33.33%] pl-[3px]">{item.sensor5}</div>
+                        <div className="table-cell tablet:text-[20px] text-[18px] break-words w-[20%] pl-[3px]">{item.sensor1}</div>
+                        <div className="table-cell tablet:text-[20px] text-[18px] break-words w-[20%] pl-[3px]">{item.sensor2}</div>
+                        <div className="table-cell tablet:text-[20px] text-[18px] break-words w-[20%] pl-[3px]">{item.sensor3}</div>
+                        <div className="table-cell tablet:text-[20px] text-[18px] break-words w-[20%] pl-[3px]">{item.sensor4}</div>
                     </div>
             )
         })
     }
 
-    useEffect(async ()=>{
-        const response= await fetchData()
-        setData(response.results)
-      },[data])
+    useEffect(async()=>{
+        let response= setInterval(async()=>{
+            let response= await fetchData()
+            setData(response.results.slice(0,10))
+        }, 5000)
+
+        return ()=> clearInterval(response)
+    }, [])
+
     
+
     return (
     <div>
-        <div class="table w-full tablet:w-[60%]">
-                <div class="table-header-group">
-                    <div class="table-row w-[100%]">
-                        <div class="table-cell tablet:text-[20px] font-bold text-[#0E4E48] text-[18px] w-[20%] pl-[3px]">sensor1</div>
-                        <div class="table-cell tablet:text-[20px] font-bold text-[#0E4E48] text-[18px] w-[20%] pl-[3px]">sensor2</div>
-                        <div class="table-cell tablet:text-[20px] font-bold text-[#0E4E48] text-[18px] w-[20%] pl-[3px]">sensor3</div>
-                        <div class="table-cell tablet:text-[20px] font-bold text-[#0E4E48] text-[18px] w-[20%] pl-[3px]">sensor4</div>
-                        <div class="table-cell tablet:text-[20px] font-bold text-[#0E4E48] text-[18px] w-[20%] pl-[3px]">sensor5</div>
+        <div className="table w-full tablet:w-[60%]">
+                <div className="table-header-group">
+                    <div className="table-row w-[100%]">
+                        <div className="table-cell tablet:text-[20px] font-bold text-[#0E4E48] text-[18px] w-[20%] pl-[3px]">sensor1</div>
+                        <div className="table-cell tablet:text-[20px] font-bold text-[#0E4E48] text-[18px] w-[20%] pl-[3px]">sensor2</div>
+                        <div className="table-cell tablet:text-[20px] font-bold text-[#0E4E48] text-[18px] w-[20%] pl-[3px]">sensor3</div>
+                        <div className="table-cell tablet:text-[20px] font-bold text-[#0E4E48] text-[18px] w-[20%] pl-[3px]">sensor4</div>
                     </div>
                 </div>
-                {renderData()}
+                {renderData(data)}
         </div>
     </div>
   )
